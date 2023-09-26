bd31828c7ea09fc15976d4d726e89386cdcebd96
Harshpandey9156 <Harshpandey21012@gmail.com>
2023-09-27 03:23:23 +0530

    update

--- CodeX Learner/LoginPage/login.php
+++ CodeX Learner/LoginPage/login.php
@@ -4,14 +4,18 @@
 $login=false;
 $showerror=false;
 if($_SERVER['REQUEST_METHOD'] == "POST"){
+
+//connection to database 
     $servername="localhost";
     $username="root";
     $password="";
     $database="project";
     
+
     $conn=mysqli_connect($servername ,$username , $password , $database);
   $email=$_POST["email"];
   $password=$_POST["password"];
+//Matching the login credentials from the database
 
 $sql= "SELECT * FROM `login` WHERE Email='$email' And Passward ='$password';";
 $result=mysqli_query($conn, $sql);
@@ -25,6 +29,7 @@ header("location: Home.html");
 
 }
 else{
+  
   echo '<div class="alert alert-danger alert-dismissible fade show" role="alert">
   <strong>Incorrect username or password </strong>
   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
