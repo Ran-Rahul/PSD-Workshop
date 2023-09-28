68784660d1590a12e2a6c8a12eb69a71d1be463e
MOON-CARVER <126223451+MOON-CARVER@users.noreply.github.com>
2023-09-29 00:16:56 +0530

    deleted flask

--- New folder/website/static/js/highscores.js
+++
@@ -1,8 +0,0 @@
-const highScoresList = document.getElementById('highScoresList'); 
-const highScores = JSON.parse(localStorage.getItem('highscores')) || []; 
-
-highScoresList.innerHTML = highScores
-    .map(score => {
-        return `<li class="high-score">${score.name} :: ${score.score}</li>`;
-    })
-    .join(""); 
